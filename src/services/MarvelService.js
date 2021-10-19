e173e05c8705f171dfeeef193a0525c766670e8d
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-10-19 21:38:16 +0300

    Improve transforming character's description

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -27,7 +27,11 @@ class MarvelService {
   _transformCharacter = (char) => {
     return {
       name: char.name,
-      description: char.description,
+      description: !char.description
+        ? 'There is no description about this character.'
+        : char.description.length > 210
+        ? char.description.slice(0, 210) + '...'
+        : char.description,
       thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
       homepage: char.urls[0].url,
       wiki: char.urls[1].url,
