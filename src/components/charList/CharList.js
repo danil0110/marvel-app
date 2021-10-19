4379db8827941911bfc45be22f4cdcdcceb67033
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-10-19 23:06:51 +0300

    Add character list fetching

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,51 +1,57 @@
+import { Component } from 'react';
+
 import './charList.scss';
 import abyss from '../../resources/img/abyss.jpg';
+import Spinner from '../spinner/Spinner';
+import MarvelService from '../../services/MarvelService';
 
-const CharList = () => {
+class CharList extends Component {
+  state = {
+    charList: [],
+    loading: true,
+  };
+
+  marvelService = new MarvelService();
+
+  componentDidMount() {
+    this.marvelService.getAllCharacters().then(this.onCharactersLoaded);
+  }
+
+  onCharactersLoaded = (charList) => {
+    this.setState({ charList, loading: false });
+  };
+
+  render() {
+    const { charList, loading } = this.state;
+    const content = loading ? <Spinner /> : <View charList={charList} />;
+
+    return <div className='char__list'>{content}</div>;
+  }
+}
+
+const View = ({ charList }) => {
   return (
-    <div className='char__list'>
+    <>
       <ul className='char__grid'>
-        <li className='char__item'>
-          <img src={abyss} alt='abyss' />
-          <div className='char__name'>Abyss</div>
-        </li>
-        <li className='char__item char__item_selected'>
-          <img src={abyss} alt='abyss' />
-          <div className='char__name'>Abyss</div>
-        </li>
-        <li className='char__item'>
-          <img src={abyss} alt='abyss' />
-          <div className='char__name'>Abyss</div>
-        </li>
-        <li className='char__item'>
-          <img src={abyss} alt='abyss' />
-          <div className='char__name'>Abyss</div>
-        </li>
-        <li className='char__item'>
-          <img src={abyss} alt='abyss' />
-          <div className='char__name'>Abyss</div>
-        </li>
-        <li className='char__item'>
-          <img src={abyss} alt='abyss' />
-          <div className='char__name'>Abyss</div>
-        </li>
-        <li className='char__item'>
-          <img src={abyss} alt='abyss' />
-          <div className='char__name'>Abyss</div>
-        </li>
-        <li className='char__item'>
-          <img src={abyss} alt='abyss' />
-          <div className='char__name'>Abyss</div>
-        </li>
-        <li className='char__item'>
-          <img src={abyss} alt='abyss' />
-          <div className='char__name'>Abyss</div>
-        </li>
+        {charList.map((item) => {
+          const pathArr = item.thumbnail.split('/');
+          const style =
+            pathArr[pathArr.length - 1] === 'image_not_available.jpg'
+              ? { objectFit: 'contain' }
+              : null;
+
+          return (
+            <li className='char__item' key={item.id}>
+              <img style={style} src={item.thumbnail} alt={item.name} />
+              <div className='char__name'>{item.name}</div>
+            </li>
+          );
+        })}
       </ul>
       <button className='button button__main button__long'>
         <div className='inner'>load more</div>
       </button>
-    </div>
+    </>
   );
 };
 
