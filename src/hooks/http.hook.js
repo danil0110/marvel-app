fc92be34e1449548310ae2ddaeb06465c804a4b3
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-10-24 18:05:31 +0300

    Add http hook

---
+++ src/hooks/http.hook.js
@@ -0,0 +1,32 @@
+import { useState, useCallback } from 'react';
+
+export const useHttp = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const request = useCallback(
+    async (url, method = 'GET', body = null, headers = { 'Content-Type': 'application/json' }) => {
+      setLoading(true);
+      try {
+        const response = await fetch(url, { method, body, headers });
+
+        if (!response.ok) {
+          throw new Error(`Couldn't fetch the ${url}. Status: ${response.status}.`);
+        }
+
+        const data = await response.json();
+        setLoading(false);
+        return data;
+      } catch (e) {
+        setLoading(false);
+        setError(e.message);
+        throw e;
+      }
+    },
+    []
+  );
+
+  const clearError = useCallback(() => setError(null));
+
+  return { loading, request, error, clearError };
+};
