3ea12b5a3a5dafbb1da74d6d6d83cbb49c118ee1
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-10-29 00:37:52 +0300

    Add FSM in SearchChar form

--- src/components/searchChar/SearchChar.js
+++ src/components/searchChar/SearchChar.js
@@ -1,20 +1,43 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as yup from 'yup';
 
-import './searchChar.scss';
 import Spinner from '../spinner/Spinner';
 import useMarvelService from '../../services/MarvelService';
 
+import './searchChar.scss';
+
+const setContent = (process, Component, data) => {
+  switch (process) {
+    case 'waiting':
+      return null;
+    case 'loading':
+      return (
+        <div className='wide'>
+          <Spinner />
+        </div>
+      );
+    case 'confirmed':
+      return data ? (
+        <Component data={data} />
+      ) : (
+        <div className='error wide'>The character was not found. Check the name and try again.</div>
+      );
+    case 'error':
+      return <ErrorMessage />;
+    default:
+      throw new Error('Unexpected process state');
+  }
+};
+
 const SearchChar = () => {
   const [char, setChar] = useState();
-  const [isInitial, setIsInitial] = useState(true);
-  const { loading, error, getCharacterByName, clearError } = useMarvelService();
+  const { loading, getCharacterByName, clearError, process, setProcess } = useMarvelService();
 
   const onCharLoaded = (char) => {
     setChar(char);
-    setIsInitial(false);
+    setProcess('confirmed');
   };
 
   const searchChar = async (name) => {
@@ -23,12 +46,6 @@ const SearchChar = () => {
     const res = await getCharacterByName(name);
     onCharLoaded(res);
   };
-
-  const spinner = loading ? (
-    <div className='wide'>
-      <Spinner />
-    </div>
-  ) : null;
 
   return (
     <Formik
@@ -47,18 +64,17 @@ const SearchChar = () => {
           <button type='submit' className='button button__main' disabled={loading}>
             <div className='inner'>find</div>
           </button>
-          {spinner}
-          {loading || isInitial ? null : char ? (
-            <>
-              <div className='success'>There is! Visit {char.name} page?</div>
-              <Link className='button button__secondary' to={`/characters/${char.id}`}>
-                <div className='inner'>Visit</div>
-              </Link>
-            </>
-          ) : (
-            <div className='error wide'>
-              The character was not found. Check the name and try again.
-            </div>
+          {setContent(
+            process,
+            () => (
+              <>
+                <div className='success'>There is! Visit {char.name} page?</div>
+                <Link className='button button__secondary' to={`/characters/${char.id}`}>
+                  <div className='inner'>Visit</div>
+                </Link>
+              </>
+            ),
+            char
           )}
           <ErrorMessage name='name' className='message error wide' component='div' />
         </div>
