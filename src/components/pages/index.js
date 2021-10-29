97aed3bccc8e7b5490528cb6c496ad517b5145c1
danil0110 <d.zalizchuk-2025@kpi.ua>
2021-10-29 10:41:35 +0300

    Add character details page export

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -2,5 +2,6 @@ import MainPage from './MainPage';
 import ComicsPage from './ComicsPage';
 import Page404 from './404';
 import SingleComicPage from './SingleComicPage';
+import SingleCharacterPage from './SingleCharacterPage';
 
-export { MainPage, ComicsPage, SingleComicPage, Page404 };
+export { MainPage, ComicsPage, SingleComicPage, SingleCharacterPage, Page404 };
